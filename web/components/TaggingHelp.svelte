15c848ba0c74bb1c2051b051ce883395890d7c2e
Smitty <smitop2@gmail.com>
2020-08-23 10:03:33 -0400

    Turn tagging help into a list

--- web/components/TaggingHelp.svelte
+++ web/components/TaggingHelp.svelte
@@ -24,16 +24,16 @@
     <span tabindex="0" class="fake-link" role="button" on:click={expanded ? depand : expand}>{expanded ? "Minimize" : "More info"}</span>
     {#if expanded}
         <div class="expanded">
-            You can include any number (including zero) of tags.
-            Tags are seperated by spaces.
-            Extra whitespace is ignored.
-            Duplicate tags are ignored.
-            Tags are automatically converted to lowercase.
-            If you input a double quote, it will be replaced with the last ping's tags.
-            You can use a single double quote to repeat last ping's tags,
-            or a double quote followed by more tags to do last ping's tags, and some additional
-            ones.
-            The order of tags does not matter.
+            <ul>
+                <li>You can include any number (including zero) of tags.</li>
+                <li>Tags are seperated by spaces.</li>
+                <li>Extra whitespace is ignored.</li>
+                <li>Duplicate tags are ignored.</li>
+                <li>Tags are automatically converted to lowercase.</li>
+                <li>If you input a double quote, it will be replaced with the last ping's tags.</li>
+                <li>You can use a single or double quote to repeat last ping's tags.</li>
+                <li>The order of tags does not matter.</li>
+            </ul>
         </div>
     {/if}
 </div>
