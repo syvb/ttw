c206d758336636ae598211f8020b82513c46f408
Smitty <smitop2@gmail.com>
2020-08-24 15:41:31 -0400

    Improve tips

--- web/tips.ts
+++ web/tips.ts
@@ -2,12 +2,16 @@ export let tips = `Keep your tags short and simple.
 Tags are converted to lowercase.
 Each tag has a unique color.
 You can export and import from the original TagTime.
-When entering tags, you can enter " (a double quote) to repeat last ping's tags.
+When entering tags, you can enter a double or single quote to repeat last ping's tags.
 You can set a sound to play when it's ping time in Settings.
 You can switch to dark mode in Settings.
 You can switch between dark mode and light mode in Settings.
 You can change the default AFK tags in settings.
-Tags are converted to lowercase.`.split("\n");
+Tags are converted to lowercase.
+On most recent browsers, you can use TagTime Web offline.
+You can export your tags as a SQLite database in Settings.
+You can enable notifications from Settings to get a push notification when it's ping time.
+When entering tags, you can click on a tag to remove it.`.split("\n");
 
 export function randTip() {
     return tips[Math.floor(Math.random() * tips.length)];
