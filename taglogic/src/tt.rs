f686972cfceb042d5982fc1697558d69a4499048
Smitty <smitop2@gmail.com>
2020-08-26 15:30:56 -0400

    Add (failing) test against first 250

--- taglogic/src/tt.rs
+++ taglogic/src/tt.rs
@@ -4,6 +4,11 @@
 
 /// Effective start of time.
 pub const UR_PING: u64 = 1184097393;
+pub const UNIV_SCHED: super::PingIntervalData = super::PingIntervalData {
+    seed: 11193462,
+    avg_interval: 2700, // 45 minutes
+    alg: super::PingAlg::TagTime,
+};
 const IA: f64 = 16807.0;
 const IM_U32: u32 = 2147483647;
 const IM_F64: f64 = 2147483647.0;
@@ -37,5 +42,32 @@ impl State {
     /// Returns the integer number of seconds until the next ping.
     pub fn gap(&self, avg_interval: u32) -> u32 {
         (self.exp_rand(avg_interval).round() as u32).max(1)
+    }
+}
+
+mod test {
+    #[allow(unused_imports)]
+    use super::*; // compiler bug cause it to think this is unused, incorrectly
+
+    #[test]
+    fn correct_first_pings() {
+        // on https://tagtime.glitch.me/ run:
+        // var x = ""; init(URPING); for (let i = 0; i < 250; i++) { x += nextping() + "\n" }; console.log(x)
+        let lines = include_str!("tagtime_first_250.txt");
+        let time_iter = lines
+            .split_ascii_whitespace()
+            .filter(|line| !line.is_empty())
+            .map(|line| line.parse::<u64>().unwrap())
+            .enumerate();
+        let mut pung = UR_PING;
+        let mut state = State::from_seed(UNIV_SCHED.seed);
+        for (index, time) in time_iter {
+            // initial ping not included in test file
+            let gap = state.gap(UNIV_SCHED.avg_interval);
+            pung += u64::from(gap);
+            assert_eq!(pung, time, "diverged on {}th ping from start", index + 1);
+            state.next_state();
+        }
+
     }
 }
