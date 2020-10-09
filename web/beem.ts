8a93a39566a9a14905b83d36204bcf4ec853c74f
Smitty <smitop2@gmail.com>
2020-10-09 16:52:07 -0400

    ditch road

--- web/beem.ts
+++ web/beem.ts
@@ -2,9 +2,14 @@
 const config = require("../config.json");
 const BEEM_URI = config.overrideBeem ?? "https://www.beeminder.com";
 
-export function authUrl() {
-    const redirUri = `${config["root-domain"]}/`;
+export function authUri() {
+    const redirUri = `${config["root-domain"]}/goals`;
     return `${BEEM_URI}/apps/authorize?client_id=${config["beem-client"]}&redirect_uri=${redirUri}&response_type=token`;
+}
+
+export function beeRedir() {
+    const uri = authUri();
+    location.href = uri;
 }
 
 export async function beemLoadCheck() {
@@ -13,7 +18,7 @@ export async function beemLoadCheck() {
     const username = params.get("username");
     if (!token || !username) return;
     // ...?
-    const beeInfo = await (await fetch(`${BEEM_URI}/api/v1/users/me.json?auth_token=${token}`)).json();
+    const beeInfo = await (await fetch(`${BEEM_URI}/api/v1/users/me.json?access_token=${token}`)).json();
     if (beeInfo.errors) {
         alert(`There was an error logging in with Beeminder: ${beeInfo.errors.message}`)
         return;
@@ -21,5 +26,6 @@ export async function beemLoadCheck() {
     if (beeInfo.username !== username) {
         alert(`BTW, Beeminder thinks your username is "${beeInfo.username}", but the URL params say it's "${username}". Weird.`);
     }
-    alert(`Sucessfully authenticated with Beeminder as ${username}.`)
+    alert(`Sucessfully authenticated with Beeminder as ${username}.`);
+    history.pushState(null, "Goals", location.pathname); // remove query params for reload
 }
--- web/components/GoalPage.svelte
+++ web/components/GoalPage.svelte
@@ -3,22 +3,6 @@
     import { Link } from "svelte-routing";
     import { getGraph } from "../goals.ts";
     import GoalSettings from "./GoalSettings.svelte";
-
-    const bgraphPromise = import("../../road/src/bgraph.js");
-    let graphContainer;
-    let id = (new URLSearchParams(location.search)).get("id");
-    let goalDataPromise = window.db.goals.get(id);
-    const mountPromise = new Promise((resolve, reject) => onMount(resolve));
-    function updateGraph(goalData, bgraph) {
-        getGraph(goalData, graphContainer, bgraph.default);
-    }
-    Promise.all([ goalDataPromise, bgraphPromise, mountPromise ]).then(([ goalData, bgraph ]) => {
-        updateGraph(goalData, bgraph);
-    });
-    async function update(e) {
-        goalDataPromise = Promise.resolve(e.detail);
-        updateGraph(e.detail, await bgraphPromise);
-    }
 </script>
 
 <Link to="/goals">Back to all goals</Link>
@@ -28,10 +12,7 @@
 {:then goal}
     {#if goal}
         <h1>{goal.name}</h1>
-        {#if goal.genGraph}
-            <div bind:this={graphContainer}></div>
-        {/if}
-        <GoalSettings on:update={update} {goal} />
+        TODO
     {:else}
         <div>
             Sorry, a goal couldn't be found with that ID.
