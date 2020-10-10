ce5500817b612bb5d4d2abfadf6bc045330189aa
Smitty <smitop2@gmail.com>
2020-10-10 09:04:53 -0400

    allow deleting goals

--- web/components/GoalPage.svelte
+++ web/components/GoalPage.svelte
@@ -1,16 +1,34 @@
 <script>
     import { onMount } from "svelte";
-    import { Link } from "svelte-routing";
+    import { Link, navigate } from "svelte-routing";
     import { getGraph } from "../goals.ts";
     import GoalSettings from "./GoalSettings.svelte";
 
     let id = (new URLSearchParams(location.search)).get("id");
     let goalDataPromise = window.db.goals.get(id);
+
+    async function deleteGoal() {
+        if (!confirm("Are you sure you want to delete this goal? If you have an associated Beeminder goal, the Beeminder goal won't be deleted.")) return;
+        await window.db.goals.delete(id);
+        navigate("/goals");
+    }
 </script>
 
 <style>
     #maincontent {
         margin: 8px;
+    }
+
+    .del-container {
+        margin-top: 5rem;
+    }
+
+    .del {
+        color: rgb(95, 11, 11);
+    }
+
+    :global(.dark) .del {
+        color: rgb(214, 171, 171);
     }
 </style>
 
@@ -22,7 +40,7 @@
     {:then goal}
         {#if goal}
             <h1>{goal.name}</h1>
-            TODO
+            <GoalSettings {goal} />
         {:else}
             <div>
                 Sorry, a goal couldn't be found with that ID.
@@ -30,4 +48,8 @@
             </div>
         {/if}
     {/await}
+
+    <div class="del-container">
+        <button class="del" on:click={deleteGoal}>Delete this goal</button>
+    </div>
 </main>
