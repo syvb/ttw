9c29beea7fcedab7efcc475740b738ac156911c8
Smitty <smitop2@gmail.com>
2020-11-06 10:29:37 -0500

    Remove "Another tip?" button
    Button was cluttering the homepage. You can still get another tip by
    clicking on the current tip.

--- web/components/Tip.svelte
+++ web/components/Tip.svelte
@@ -9,10 +9,12 @@
     .tip-start {
         font-weight: bold;
     }
+    .tip-root {
+        cursor: pointer;
+    }
 </style>
 
-<div>
+<div class="tip-root" on:click={newTip}>
     <span class="tip-start">Tip!</span>
     {curTip}
-    <button on:click={newTip}>Another tip?</button>
 </div>
