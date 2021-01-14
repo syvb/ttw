e6acc325c72f21bc92a8b9b55134530895a5af63
Smitty <me@smitop.com>
2021-01-14 15:17:18 -0500

    Add UI for new suggestion system

--- web/components/DateRangePicker.svelte
+++ web/components/DateRangePicker.svelte
@@ -26,8 +26,39 @@
         });
         picker.changeYear(now.getFullYear());
     });
+    function rangeSelect(rangeType, qual) {
+        // TODO...
+    }
 </script>
 
+<style>
+    .sugg {
+        display: inline-block;
+        border-radius: 21px;
+        background: #005644;
+        cursor: pointer;
+
+        padding-top: 2px;
+        padding-bottom: 2px;
+        padding-left: 6px;
+        padding-right: 6px;
+
+        margin-bottom: 0.5rem;
+    }
+    .suggs {
+        width: 307.875px; /* flatpickr calendar width */
+    }
+</style>
+
 <div>
+    <div class="suggs">
+        <span class="sugg" on:click={rangeSelect("calRel", "today")}>Today</span>
+        <span class="sugg" on:click={rangeSelect("calRel", "week")}>This week</span>
+        <span class="sugg" on:click={rangeSelect("calRel", "month")}>This month</span>
+        <span class="sugg" on:click={rangeSelect("calRel", "year")}>This year</span>
+        <span class="sugg" on:click={rangeSelect("rel", 30)}>Last 30 days</span>
+        <span class="sugg" on:click={rangeSelect("rel", 90)}>Last 90 days</span>
+        <span class="sugg" on:click={rangeSelect("rel", 365)}>Last 365 days</span>
+    </div>
     <div bind:this={pickerEle}></div>
 </div>
