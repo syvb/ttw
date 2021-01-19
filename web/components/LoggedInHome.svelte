5493916c84b2935f41c415707693ac26c4112520
Smitty <me@smitop.com>
2021-01-19 12:06:28 -0500

    fix bg dimensions

--- web/components/LoggedInHome.svelte
+++ web/components/LoggedInHome.svelte
@@ -29,8 +29,8 @@
         position: absolute;
         top: 0;
         left: 0;
-        width: 100vw;
-        height: 100vh;
+        width: 100%;
+        height: 100%;
         background-size: cover;
         z-index: -10;
     }
