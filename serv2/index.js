a2d28491e4e314f64003645a63d56d0f5ff65eb8
Smitty <me@smitop.com>
2021-01-25 18:08:20 -0500

    Allow specifying cookie domain

--- serv2/index.js
+++ serv2/index.js
@@ -78,12 +78,17 @@ async function setAuthCookie(res, uid) {
         token: cookie,
         uid,
     });
-    res.cookie("retag-auth", cookie, {
+    const cookieData = {
         maxAge: 86400000 * 365, // 1 year
         httpOnly: true,
-        sameSite: "None",
         secure: !!config["secure-cookie"],
-    });
+    };
+    if (config["cookie-domain"]) {
+        cookieData.domain = config["cookie-domain"];
+    } else {
+        cookieData.sameSite = "None";
+    }
+    res.cookie("retag-auth", cookie, );
 }
 
 const app = express();
