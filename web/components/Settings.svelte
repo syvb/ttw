e60ac32a8d379fa3144fdecba1d9318a439bb16d
Smitty <smitop2@gmail.com>
2021-01-31 12:27:45 -0500

    style new settings ui

--- web/components/Settings.svelte
+++ web/components/Settings.svelte
@@ -226,195 +226,223 @@
     summary {
         cursor: pointer;
     }
+
+    .inner {
+        margin-left: 13rem;
+        position: relative;
+        top: -31rem;
+    }
+
+    .sidebar {
+        height: 30rem;
+        width: 13rem;
+        background: white;
+        position: sticky;
+        top: 0.5rem;
+    }
+
+    .sidebar > a {
+        display: block;
+        color: black;
+        text-decoration: none;
+        border: 2px solid #005644;
+        width: fit-content;
+        border-radius: 3rem;
+        padding: .5rem;
+        margin-bottom: 0.5rem;
+        cursor: pointer;
+    }
 </style>
 
 <svelte:window on:hashchange={updateTab} />
 
 <main id="maincontent">
-    <h2>Settings</h2>
-
-    {#if !navigator.onLine}
-        <div>
-            Note: some settings cannot be changed while offline
-        </div>
-    {/if}
-
     <div class="sidebar">
-        <a href="#ping-notifs">Pings and notifications</a>
-        <a href="#import-export">Imports/Exports</a>
-        <a href="#theme">Theme</a>
-        <a href="#account">Account</a>
-        <a href="#advanced">Advanced</a>
+        <a href="/settings#ping-notifs">Pings and notifications</a>
+        <a href="/settings#import-export">Imports/Exports</a>
+        <a href="/settings#theme">Theme</a>
+        <a href="/settings#account">Account</a>
+        <a href="/settings#advanced">Advanced</a>
     </div>
 
-    <h3>Ping notifications</h3>
-    <div>
-        <NotificationsPerm />
-    </div>
-    <div>
-        <label for="ping-noise">Audio to play on a ping: </label>
-        <!-- I *think* using `change` is okay here  -->
-        <!-- svelte-ignore a11y-no-onchange -->
-        <select id="ping-noise" bind:value={pingNoise} on:change={updatePingNoise}>
-            <option value="n">None</option>
-            <option value="d3">Simple ding</option>
-            <option value="d1">Bell Arpeggio</option>
-            <option value="d2">Doorbell</option>
-        </select>
-        <div class="ding-audio-info">
-            {#if pingNoise === "n"}
-                No audio will be played.
-            {:else if pingNoise === "d1"}
-                It kinda sounds like some windchimes.
-                Apparently an &ldquo;arpeggio&rdquo; is a type of broken chord.
-                And a &ldquo;broken chord&rdquo; is a chord but broken up and maybe with some notes repeated.
-                Audio is from <a href="https://cynicmusic.com/" rel="noopener">The Cynic Project</a>.
-            {:else if pingNoise === "d2"}
-                Sounds like a doorbell. Ding-dong! Very confusing if this device is near a door. Audio is from
-                <a href="https://freesound.org/people/MatthewWong/" rel="noopener" title="Don't get too excited about clicking this link. MatthewWong's only upload to freesound.org is this doorbell sound. Which you've already heard if you're hovering over this link.">MatthewWong</a>.
-            {:else if pingNoise === "d3"}
-                Sounds a bit futuristic (but not too futuristic).
-                Audio is from
-                <a href="https://freesound.org/people/robni7/" rel="noopener">robni7</a>.
-            {:else if pingNoise === "d1337"}
-                no
-            {:else}
-                Erm what? honestly I don't what know *what* will be played
-            {/if}
-        </div>
-        <div class="ding-info">
-            Select a ping noise to hear it.
-            {#await window.supportsAutoplay then supported}
-                {#if !supported && (pingNoise !== "n")}
-                    <div class="autoplay-warning">
-                        Note: Your browser doesn't support automatically playing audio. (it might be that you need to tap on the page before audio can play)
-                    </div>
-                {/if}
-            {/await}
-        </div>
-    </div>
+    <div class="inner">
+        <h2>Settings</h2>
 
-    <h3>Pinging</h3>
-    <div>
-        <div>
-            <label for="pint-interval">
-                Average ping interval (format like 45:12 for a ping every 45 minutes and 12 seconds, changing this will disable notifications):
-            </label>
-            <input type="text" id="pint-interval" bind:value={pintAvgInterval}>
-        </div>
-        <div>
-            {#if pintAlgChecked && (!(pintAvgInterval.trim() === "45:00" || pintAvgInterval.trim() === "45:0") || (pintSeed !== "1184097393"))}
-                Note: You are are using the original TagTime algorithm but not the universal schedule.
-                Performance will be degraded and notifications will not be sent (due to the lack of lookup tables).
-                Click the below button to use the universal schedule.
-            {/if}
-        </div>
-        <div>
-            {STR.reloadNote}
-        </div>
-        <div>
-            <button on:click={updatePintClick}>Update</button>
-        </div>
-    </div>
-    <div>
-        Default tags when multiple pending:
-        <TagEntry bind:tags={afkTags} on:input={afkTagsUpdate} small />
-    </div>
-
-    <h3>Universal Schedule (beta)</h3>
-    <div>
-        <div>
-            Click the button to use the
-            <a href="https://forum.beeminder.com/t/official-reference-implementation-of-the-tagtime-universal-ping-schedule/4282">universal schedule</a>.
-            This overides your interval, seed (in advanced settings), and algorithm (in advanced settings) to the universal schedule with just one click.
-        </div>
-        <div>
-            {STR.reloadNote}
-        </div>
-        <div>
-            <button on:click={useUnivSched}>
-                Use the universal schedule
-            </button>
-        </div>
-    </div>
-
-    <h3>Import/export</h3>
-    <div>
-        <label for="tt-import" class="tt-import-btn">
-            <button on:click={doTtImport} disabled={tagtimeImportPending}>
-                Import from TagTime
-            </button>
-        </label>
-        (Only adds pings, does not remove or overwrite existing pings. Invalid lines are ignored. Does not import interval settings. Assumes TagTime used the currently active interval)
-        <input id="tt-import" class="tt-import" aria-hidden="true" type="file" on:change={tagtimeImport} bind:this={ttImportButton} disabled={tagtimeImportPending}>
-    </div>
-    <div>
-        <button on:click={tagtimeExport} disabled={tagtimeExportPending}>Export as TagTime log</button>
-    </div>
-    <div>
-        <button on:click={dbDownload}>Download SQLite database</button>
-    </div>
-    <div>
-        <button on:click={beemResyncClick}>Resync with Beeminder</button>
-        <div>
-            Ensures Beeminder has all pings over the last 7 days.
-        </div>
-    </div>
-
-    <h3><label for="theme-dropdown">Theme</label></h3>
-    <select id="theme-dropdown" on:input={updateTheme}>
-        <option selected={theme === "default"} value="default">Browser default {(window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) ? "(dark)" : "(light)"}</option>
-        <option selected={theme === "dark"} value="dark">Dark</option>
-        <option selected={theme === "light"} value="light">Light</option>
-    </select>
-
-    <h3>Account</h3>
-    <div>
-        <a href={config["api-server"] + "/internal/changepw"}>Change password</a>
-    </div>
-
-    <h3 class="dz">Danger Zone</h3>
-    <div>
-        <button on:click={deleteAllData} class="delete-button">Delete all data</button>
-    </div>
-    <div>
-        Note: This will not delete your account. To delete your account, <a href={"mailto:" + config["contact-email"]}>contact support</a>.
-    </div>
-
-    <details class="adv">
-        <summary><h3 class="adv-settings">Advanced</h3></summary>
-        <div>
-            You probably don't want to touch any of the settings here.
-        </div>
-        <div>
-            <button on:click={tryToPersist}>Request persistent storage</button>
-        </div>
-        <div>
-            <button on:click={rebuildTagIndex}>Rebuild tag index</button>
-        </div>
-        <div>
-            <label for="pint-seed">
-                Ping seed (changing this will disable notifications, see above warning for interval):
-            </label>
-            <input type="number" id="pint-seed" bind:value={pintSeed}>
+        {#if !navigator.onLine}
             <div>
-                Note that if the box below is checked, performance will be worse if the seed isn't 1184097393. (due to lookup tables)
+                Note: some settings cannot be changed while offline
             </div>
-            <button on:click={updatePintClick}>Update</button>
+        {/if}
+
+        <h3>Ping notifications</h3>
+        <div>
+            <NotificationsPerm />
         </div>
         <div>
-            <input type="checkbox" id="pint-tt-alg" bind:checked={pintAlgChecked}>
-            <label for="pint-tt-alg">
-                Use original TagTime algorithm (beta, see below)
-            </label>
-            <details>
-                <summary>Read this before changing the above checkbox!</summary>
-                You should check this box if you want compatability with the original TagTime.
-                Checking this checkbox will enable using the algorithm used by the original TagTime.
-                You can change the seed under advanced settings. Note that UR_PING is always 1184097393.
-                To use the universal schdule, check the above checkbox, set the ping inverval to 45:00, and set the seed to 11193462 (or click the above button to do that for you).
-            </details>
-            <button on:click={updatePintClick}>Update</button>
+            <label for="ping-noise">Audio to play on a ping: </label>
+            <!-- I *think* using `change` is okay here  -->
+            <!-- svelte-ignore a11y-no-onchange -->
+            <select id="ping-noise" bind:value={pingNoise} on:change={updatePingNoise}>
+                <option value="n">None</option>
+                <option value="d3">Simple ding</option>
+                <option value="d1">Bell Arpeggio</option>
+                <option value="d2">Doorbell</option>
+            </select>
+            <div class="ding-audio-info">
+                {#if pingNoise === "n"}
+                    No audio will be played.
+                {:else if pingNoise === "d1"}
+                    It kinda sounds like some windchimes.
+                    Apparently an &ldquo;arpeggio&rdquo; is a type of broken chord.
+                    And a &ldquo;broken chord&rdquo; is a chord but broken up and maybe with some notes repeated.
+                    Audio is from <a href="https://cynicmusic.com/" rel="noopener">The Cynic Project</a>.
+                {:else if pingNoise === "d2"}
+                    Sounds like a doorbell. Ding-dong! Very confusing if this device is near a door. Audio is from
+                    <a href="https://freesound.org/people/MatthewWong/" rel="noopener" title="Don't get too excited about clicking this link. MatthewWong's only upload to freesound.org is this doorbell sound. Which you've already heard if you're hovering over this link.">MatthewWong</a>.
+                {:else if pingNoise === "d3"}
+                    Sounds a bit futuristic (but not too futuristic).
+                    Audio is from
+                    <a href="https://freesound.org/people/robni7/" rel="noopener">robni7</a>.
+                {:else if pingNoise === "d1337"}
+                    no
+                {:else}
+                    Erm what? honestly I don't what know *what* will be played
+                {/if}
+            </div>
+            <div class="ding-info">
+                Select a ping noise to hear it.
+                {#await window.supportsAutoplay then supported}
+                    {#if !supported && (pingNoise !== "n")}
+                        <div class="autoplay-warning">
+                            Note: Your browser doesn't support automatically playing audio. (it might be that you need to tap on the page before audio can play)
+                        </div>
+                    {/if}
+                {/await}
+            </div>
         </div>
-    </details>
+
+        <h3>Pinging</h3>
+        <div>
+            <div>
+                <label for="pint-interval">
+                    Average ping interval (format like 45:12 for a ping every 45 minutes and 12 seconds, changing this will disable notifications):
+                </label>
+                <input type="text" id="pint-interval" bind:value={pintAvgInterval}>
+            </div>
+            <div>
+                {#if pintAlgChecked && (!(pintAvgInterval.trim() === "45:00" || pintAvgInterval.trim() === "45:0") || (pintSeed !== "1184097393"))}
+                    Note: You are are using the original TagTime algorithm but not the universal schedule.
+                    Performance will be degraded and notifications will not be sent (due to the lack of lookup tables).
+                    Click the below button to use the universal schedule.
+                {/if}
+            </div>
+            <div>
+                {STR.reloadNote}
+            </div>
+            <div>
+                <button on:click={updatePintClick}>Update</button>
+            </div>
+        </div>
+        <div>
+            Default tags when multiple pending:
+            <TagEntry bind:tags={afkTags} on:input={afkTagsUpdate} small />
+        </div>
+
+        <h3>Universal Schedule (beta)</h3>
+        <div>
+            <div>
+                Click the button to use the
+                <a href="https://forum.beeminder.com/t/official-reference-implementation-of-the-tagtime-universal-ping-schedule/4282">universal schedule</a>.
+                This overides your interval, seed (in advanced settings), and algorithm (in advanced settings) to the universal schedule with just one click.
+            </div>
+            <div>
+                {STR.reloadNote}
+            </div>
+            <div>
+                <button on:click={useUnivSched}>
+                    Use the universal schedule
+                </button>
+            </div>
+        </div>
+
+        <h3>Import/export</h3>
+        <div>
+            <label for="tt-import" class="tt-import-btn">
+                <button on:click={doTtImport} disabled={tagtimeImportPending}>
+                    Import from TagTime
+                </button>
+            </label>
+            (Only adds pings, does not remove or overwrite existing pings. Invalid lines are ignored. Does not import interval settings. Assumes TagTime used the currently active interval)
+            <input id="tt-import" class="tt-import" aria-hidden="true" type="file" on:change={tagtimeImport} bind:this={ttImportButton} disabled={tagtimeImportPending}>
+        </div>
+        <div>
+            <button on:click={tagtimeExport} disabled={tagtimeExportPending}>Export as TagTime log</button>
+        </div>
+        <div>
+            <button on:click={dbDownload}>Download SQLite database</button>
+        </div>
+        <div>
+            <button on:click={beemResyncClick}>Resync with Beeminder</button>
+            <div>
+                Ensures Beeminder has all pings over the last 7 days.
+            </div>
+        </div>
+
+        <h3><label for="theme-dropdown">Theme</label></h3>
+        <select id="theme-dropdown" on:input={updateTheme}>
+            <option selected={theme === "default"} value="default">Browser default {(window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) ? "(dark)" : "(light)"}</option>
+            <option selected={theme === "dark"} value="dark">Dark</option>
+            <option selected={theme === "light"} value="light">Light</option>
+        </select>
+
+        <h3>Account</h3>
+        <div>
+            <a href={config["api-server"] + "/internal/changepw"}>Change password</a>
+        </div>
+
+        <h3 class="dz">Danger Zone</h3>
+        <div>
+            <button on:click={deleteAllData} class="delete-button">Delete all data</button>
+        </div>
+        <div>
+            Note: This will not delete your account. To delete your account, <a href={"mailto:" + config["contact-email"]}>contact support</a>.
+        </div>
+
+        <details class="adv">
+            <summary><h3 class="adv-settings">Advanced</h3></summary>
+            <div>
+                You probably don't want to touch any of the settings here.
+            </div>
+            <div>
+                <button on:click={tryToPersist}>Request persistent storage</button>
+            </div>
+            <div>
+                <button on:click={rebuildTagIndex}>Rebuild tag index</button>
+            </div>
+            <div>
+                <label for="pint-seed">
+                    Ping seed (changing this will disable notifications, see above warning for interval):
+                </label>
+                <input type="number" id="pint-seed" bind:value={pintSeed}>
+                <div>
+                    Note that if the box below is checked, performance will be worse if the seed isn't 1184097393. (due to lookup tables)
+                </div>
+                <button on:click={updatePintClick}>Update</button>
+            </div>
+            <div>
+                <input type="checkbox" id="pint-tt-alg" bind:checked={pintAlgChecked}>
+                <label for="pint-tt-alg">
+                    Use original TagTime algorithm (beta, see below)
+                </label>
+                <details>
+                    <summary>Read this before changing the above checkbox!</summary>
+                    You should check this box if you want compatability with the original TagTime.
+                    Checking this checkbox will enable using the algorithm used by the original TagTime.
+                    You can change the seed under advanced settings. Note that UR_PING is always 1184097393.
+                    To use the universal schdule, check the above checkbox, set the ping inverval to 45:00, and set the seed to 11193462 (or click the above button to do that for you).
+                </details>
+                <button on:click={updatePintClick}>Update</button>
+            </div>
+        </details>
+    </div>
 </main>
