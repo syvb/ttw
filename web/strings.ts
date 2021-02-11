770e669b5b17d88763f2ba0fa70056541a11ce1b
Smitty <me@smitop.com>
2021-02-11 15:18:58 -0500

    Update tips

--- web/strings.ts
+++ web/strings.ts
@@ -14,8 +14,10 @@ Each tag has a unique color.
 You can export and import from the original TagTime.
 When entering tags, you can enter a double or single quote to repeat last ping's tags.
 You can set a sound to play when it's ping time in Settings.
-You can switch to dark mode in Settings.
 You can switch between dark mode and light mode in Settings.
+You can switch to the original TagTime algorithm in Settings.
+You can use filtering options in the Pings/Goals/Graphs tab to only include pings without a certain tag.
+You can use filtering options in the Pings/Goals/Graphs tab to include pings that have all of a list of tags.
 You can change the default AFK tags in settings.
 Tags are converted to lowercase.
 On most recent browsers, you can use ${STR.appName} offline.
@@ -24,7 +26,8 @@ You can enable notifications from Settings to get a push notification when it's 
 When entering tags, you can click on a tag to remove it.
 You can connect ${STR.appName} to Beeminder in the Goals tab.
 You can see what hours of the day get the most of a particular tag with the Daily distribution graph.
-${STR.appName} is open source! You can find in on GitHub as smittyvb/ttw`.split("\n");
+${STR.appName} is open source! You can find in on GitHub as smittyvb/ttw
+You can easily see all pings over a 30 day time range in the Pings and Graphs tab.`.split("\n");
 
 STR.welcome = `Welcome to ${STR.appName}!`;
 
