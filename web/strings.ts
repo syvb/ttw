ae8a889f3443dbc27f8c9f306d1836a6408ce55b
Smitty <me@smitop.com>
2021-03-06 10:14:34 -0500

    more tips

--- web/strings.ts
+++ web/strings.ts
@@ -16,8 +16,8 @@ When entering tags, you can enter a double or single quote to repeat last ping's
 You can set a sound to play when it's ping time in Settings.
 You can switch between dark mode and light mode in Settings.
 You can switch to the original TagTime algorithm in Settings.
-You can use filtering options in the Pings/Goals/Graphs tab to only include pings without a certain tag.
-You can use filtering options in the Pings/Goals/Graphs tab to include pings that have all of a list of tags.
+You can use filtering options to only include pings without a certain tag.
+You can use filtering options to include pings that have all of a list of tags.
 You can change the default AFK tags in settings.
 Tags are converted to lowercase.
 On most recent browsers, you can use ${STR.appName} offline.
@@ -27,6 +27,8 @@ You can enable notifications from Settings to get a push notification when it's 
 When entering tags, you can click on a tag to remove it.
 You can connect ${STR.appName} to Beeminder in the Goals tab.
 You can see what hours of the day get the most of a particular tag with the Daily distribution graph.
+You can see a matrix graph of your time in the Graphs tab.
+How do you spend the hours of your day/week/month? Find out in the Graphs tab.
 ${STR.appName} is open source! You can find in on GitHub as smittyvb/ttw
 You can easily see all pings over a 30 day time range in the Pings and Graphs tab.`.split("\n");
 
