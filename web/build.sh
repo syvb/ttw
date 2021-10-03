60ad73270f6a245e8f0e688a54291a0845d3d34f
Smitty <me@smitop.com>
2021-10-03 14:52:02 -0400

    Insert SA script on index

--- web/build.sh
+++ web/build.sh
@@ -3,7 +3,7 @@ yarn licenses generate-disclaimer --silent --prod > static/disclaimer.txt
 
 echo Templating pages
 sed "s/APPNAME/$(node -e "console.log(require('../config.json')['app-name'] || 'TagTime Web')")/g" static/manifest.webmanifest.template > static/manifest.webmanifest
-sed "s/APPNAME/$(node -e "console.log(require('../config.json')['app-name'] || 'TagTime Web')")/g" static/index.html.template | sed "s*BACKSERV*$(node -e "console.log(require('../config.json')['api-server'])")*g" | sed "s*EXTRA_TEXT*$(node -e "console.log(require('../config.json')['extra-homepage-text'])")*g" > static/index.html
+sed "s/APPNAME/$(node -e "console.log(require('../config.json')['app-name'] || 'TagTime Web')")/g" static/index.html.template | sed "s*BACKSERV*$(node -e "console.log(require('../config.json')['api-server'])")*g" | sed "s*EXTRA_TEXT*$(node -e "let c = require('../config.json');console.log(c['extra-homepage-text']);if(c.enableSimpleAnalytics)console.log('<script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>')")*g" > static/index.html
 
 echo Compiling taglogic
 rm -rf dist pkg
