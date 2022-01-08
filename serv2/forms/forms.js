45311039acc130182ec0bcda53b1b960c847a9fa
Smitty <me@smitop.com>
2022-01-08 18:02:27 -0500

    add librejs licensing info for forms

--- serv2/forms/forms.js
+++ serv2/forms/forms.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:8e4f440f4c65981c5bf93c76d35135ba5064d8b7&dn=apache-2.0.txt Apache-2.0
 window.addEventListener("DOMContentLoaded", function() {
     for (let i = 0; i < document.forms.length; i++) {
         const form = document.forms[i];
@@ -6,3 +7,4 @@ window.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+// @license-end
